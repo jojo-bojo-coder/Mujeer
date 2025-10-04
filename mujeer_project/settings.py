2778018daa2665a8f4e554fdb741521557bd2276
jojo cabanojo <receptionist402@gmail.com>
2025-10-05 02:16:53 +0300

    Update settings.py

--- mujeer_project/settings.py
+++ mujeer_project/settings.py
@@ -145,6 +145,7 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Media files
 MEDIA_URL = '/media/'
